a9bcf132408b69964922d7a4c36ec4e6f5219b9c
emttiew <emttiew@gmail.com>
2020-03-17 20:01:50 +0100

    Rozwiązanie zadania 03

--- Rozdzial11/zad03/randwalk.cpp
+++ Rozdzial11/zad03/randwalk.cpp
@@ -16,6 +16,9 @@ int main()
     unsigned long steps = 0;
     double target;
     double dstep;
+    unsigned long attempts;
+    unsigned long min, max, sum = 0;
+
     cout << "Podaj dystans do przejścia (k, aby zakończyć): ";
     while (cin >> target)
     {
@@ -23,23 +26,35 @@ int main()
         if (!(cin >> dstep))
             break;
 
-        while (result.magval() < target)
+        cout << "Podaj ilość prób: ";
+        if (!(cin >> attempts))
+            break;
+
+        for (int i = 0; i < attempts; i++) 
         {
-            direction = rand() % 360;
-            step.reset(dstep, direction, Vector::POL);
-            result = result + step;
-            steps++;
+            while (result.magval() < target)
+            {
+                direction = rand() % 360;
+                step.reset(dstep, direction, Vector::POL);
+                result = result + step;
+                steps++;
+            }
+            if (i == 0)
+                max = min = steps;
+
+            if (steps > max)
+				max = steps;
+			if (steps < min)
+				min = steps;
+
+            sum += steps;            
+            steps = 0;
+            result.reset(0.0, 0.0);                      
         }
-        cout << "Po " << steps << " krokach delikwent "
-            "osiągnął położenie:\n";
-        cout << result << endl;
-        result.polar_mode();
-        cout << " czyli\n" << result << endl;
-        cout << "Średnia długość kroku pozornego = "
-            << result.magval()/steps << endl;
-        steps = 0;
-        result.reset(0.0, 0.0);
-        cout << "Podaj dystans do przejścia (k, aby zakończyć): ";
+            cout << "Po " << attempts << " próbach minimalna ilość przebytych kroków to: " << min << endl;
+            cout << "Maksymalna ilość kroków to: " << max << endl;
+            cout << "Średnia ilość kroków potrzebna do przebycia zadanego dystansu: " << sum/attempts << endl;
+            cout << "Podaj dystans do przejścia (k, aby zakończyć): ";
     }
     cout << "Koniec!\n";
     cin.clear();
