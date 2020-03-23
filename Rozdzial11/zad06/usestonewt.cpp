04509dac1d92e1fe62c2bec1973db74100b194a8
emttiew <emttiew@gmail.com>
2020-03-23 16:07:36 +0100

    TBD

---
+++ Rozdzial11/zad06/usestonewt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+#include "stonewt.h"
+
+
+int main()
+{
+    const int ArrSize = 6;
+
+    Stonewt tab[ArrSize];
+    tab[0] = 10;
+    tab[1] = 11;
+    tab[2] = 13;
+
+
+}
