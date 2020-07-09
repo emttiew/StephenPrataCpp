1d5cfa89f429b9f8cb0770752cad69d780cf1c44
emttiew <emttiew@gmail.com>
2020-07-09 22:54:37 +0200

    pe14-5 - code form book

---
+++ Rozdzial14/zad5/pe14-5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+#include "emp.h"
+
+int main(void)
+{
+    employee em("Tadeusz", "Hubert", "Sprzedawca");
+    cout << em << endl;
+    em.ShowAll();
+
+    manager ma("Amforiusz", "Smoczewski", "Malarz", 5);
+    cout << ma << endl;
+    ma.ShowAll();
+
+
+    fink fi("Maurycy", "Olkuski", "Hydraulik", "Julian Bar");
+    cout << fi << endl;
+    fi.ShowAll();
+    highfink hf(ma, "Jan Kudłaty");  
+    hf.ShowAll();
+    cout << "Wciśnij dowolny przycisk, aby przejść do następnego etapu:\n";
+    cin.get();
+    highfink hf2;
+    hf2.SetAll();
+
+    cout << "Używa wskaźnika abstr_emp *:\n";
+    abstr_emp  * tri[4] = {&em, &fi, &hf, &hf2};
+    for (int i = 0; i < 4; i++)
+        tri[i]->ShowAll();
+    return 0;
+}
